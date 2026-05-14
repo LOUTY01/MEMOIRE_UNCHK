632db5250747f73f00c97c5af79223e039161e40
Dian2diallo <diallodian2448@gmail.com>
2026-05-14 19:43:25 +0200

    d

---
+++ you.php
@@ -0,0 +1,7 @@
+<?php
+  // Ceci est un commentaire
+  echo "Hello World !"; // Affiche du texte
+  
+  $nom = "Dev"; // Déclaration d'une variable
+  echo "Bonjour " . $nom; // Concaténation avec un point (.)
+?>
