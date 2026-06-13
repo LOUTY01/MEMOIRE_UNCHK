a2cf5a3f69a8d307abad18165f9e06bd43a49fe4
LOUTY01 <djiby.diallo9@unchk.edu.sn>
2026-06-13 10:13:07 +0000

    Delete you.php

--- you.php
+++
@@ -1,7 +0,0 @@
-<?php
-  // Ceci est un commentaire
-  echo "Hello World !"; // Affiche du texte
-  
-  $nom = "Dev"; // Déclaration d'une variable
-  echo "Bonjour " . $nom; // Concaténation avec un point (.)
-?>
