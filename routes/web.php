2a25c77f158b89e09bfe3a809f3d6d2036243e18
Djiby DIALLO <diverslouty@gmail.com>
2026-06-13 10:23:27 +0000

    Premier commit

---
+++ routes/web.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Routes Web
+|--------------------------------------------------------------------------
+*/
+
+Route::get('/', function () {
+    return view('accueil');
+})->name('accueil');
+
+Route::get('/services', function () {
+    return view('services');
+})->name('services');
+
+Route::get('/propos', function () {
+    return view('propos');
+})->name('propos');
+
+Route::get('/rendez-vous', function () {
+    return view('rendezvous');
+})->name('rendezvous');
+
+Route::get('/contact', function () {
+    return view('contact');
+})->name('contact');
